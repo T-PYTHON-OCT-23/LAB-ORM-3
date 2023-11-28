1566524c99e10a422cbab7770601f65f96282597
MaramAlasmari <147453012+MaramAlasmari@users.noreply.github.com>
2023-11-28 12:26:09 +0300

    done

---
+++ Blog/blogs/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    
+    categories=models.TextChoices("Categories", ["Makeup", "Movie","Celebrities", "Care","Places"])
+  
+
+    title=models.CharField(max_length=100)
+    content=models.TextField()
+    is_published=models.BooleanField()
+    published_at=models.DateField()
+    category=models.CharField(max_length=50,choices=categories.choices, default="Movie")
+    poster = models.ImageField(upload_to="images/", default="images/blog.jpg")
+
+
+
+class Review(models.Model):
+    blog = models.ForeignKey(Blog,on_delete=models.CASCADE) 
+    name=models.CharField(max_length=100)
+    date_add= models.DateTimeField(auto_now_add=True)
+    rating= models.TextField()
+    comment = models.TextField()
---
+++ Blog/main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from blogs.models import Blog ,Review
+# Create your views here.
+
+def home(request : HttpRequest):
+    
+    reviews = Review.objects.all().order_by("-date_add")[0:6]
+    return render(request,'main/home.html', {"reviews" : reviews})
+
