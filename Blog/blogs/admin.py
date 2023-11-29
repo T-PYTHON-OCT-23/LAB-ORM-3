d85396e897d682bb8f765d7950fd3b0c6f50ef25
MaramAlasmari <147453012+MaramAlasmari@users.noreply.github.com>
2023-11-29 11:21:05 +0300

    done

--- Blog/blogs/admin.py
+++ Blog/blogs/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Blog, Review
 # Register your models here.
+
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display=('title','published_at','category',)
+    list_filter=('category',)
+
+
+admin.site.register(Blog,BlogAdmin)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display= ('name','date_add','rating',)
+    list_filter=('blog',)
+
+admin.site.register(Review,ReviewAdmin)
--- Blog/blogs/models.py
+++ Blog/blogs/models.py
@@ -13,7 +13,8 @@ class Blog(models.Model):
     category=models.CharField(max_length=50,choices=categories.choices, default="Movie")
     poster = models.ImageField(upload_to="images/", default="images/blog.jpg")
 
-
+    def __str__(self):
+        return f"{self.title}"
 
 class Review(models.Model):
     blog = models.ForeignKey(Blog,on_delete=models.CASCADE) 
@@ -21,3 +22,6 @@ class Review(models.Model):
     date_add= models.DateTimeField(auto_now_add=True)
     rating= models.TextField()
     comment = models.TextField()
+
+    def __str__(self):
+     return f"{self.name} : {self.blog}"
