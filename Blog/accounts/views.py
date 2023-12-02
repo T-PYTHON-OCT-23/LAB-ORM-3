4d55d4f7d5b79dd85065e13787e42f6200d86608
naif <iimnaif@gmail.com>
2023-12-02 17:14:16 +0300

    done

--- Blog/accounts/views.py
+++ Blog/accounts/views.py
@@ -10,7 +10,7 @@ def register_user_view(request: HttpRequest):
 
     if request.method == "POST":
         #create a new user
-        user = User.objects.create_user(username=request.POST["username"], first_name=request.POST["first_name"], last_name=request.POST["last_name"], email=request.POST["email"], password=request.POST["password"])
+        user = User.objects.create_user(username=request.POST["username"], email=request.POST["email"], password=request.POST["password"])
         user.save()
         return redirect("accounts:login_user_view")
 
